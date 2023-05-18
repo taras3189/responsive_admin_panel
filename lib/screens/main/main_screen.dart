80cf1e295e224402aea1b01a98251c3d48315de0
Taras Bobriienko <taras3189@gmail.com>
2023-05-18 13:26:41 +0300

    2 commit

---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
